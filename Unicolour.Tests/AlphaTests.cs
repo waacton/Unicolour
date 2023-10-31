c58647eccfc47c13479c9e749d82c763d8f15783
Wacton <waacton@gmail.com>
2023-10-31 23:50:52 +0000

    Remove other library tests

---
+++ Unicolour.Tests/AlphaTests.cs
@@ -0,0 +1,33 @@
+namespace Wacton.Unicolour.Tests;
+
+using NUnit.Framework;
+
+public class AlphaTests
+{
+    [TestCase(0, 0, "00")]
+    [TestCase(0.25, 64, "40")]
+    [TestCase(0.5, 128, "80")]
+    [TestCase(0.75, 191, "BF")]
+    [TestCase(1, 255, "FF")]
+    public void InRange(double value, int value255, string hex) => AssertAlpha(value, value255, hex);
+
+    [TestCase(-0.00000000001, 0, "00")]
+    [TestCase(-0.5, 0, "00")]
+    [TestCase(1.00000000001, 255, "FF")]
+    [TestCase(1.5, 255, "FF")]
+    [TestCase(double.MinValue, 0, "00")]
+    [TestCase(double.MaxValue, 255, "FF")]
+    [TestCase(double.Epsilon, 0, "00")]
+    [TestCase(double.NegativeInfinity, 0, "00")]
+    [TestCase(double.PositiveInfinity, 255, "FF")]
+    [TestCase(double.NaN, 0, "00")]
+    public void OutRange(double value, int value255, string hex) => AssertAlpha(value, value255, hex);
+
+    private void AssertAlpha(double value, int value255, string hex)
+    {
+        var alpha = new Alpha(value);
+        Assert.That(alpha.A, Is.EqualTo(value));
+        Assert.That(alpha.A255, Is.EqualTo(value255));
+        Assert.That(alpha.Hex, Is.EqualTo(hex));
+    }
+}
--- Unicolour/Rgb255.cs
+++ Unicolour/Rgb255.cs
@@ -3,12 +3,12 @@
 public record Rgb255 : ColourRepresentation
 {
     protected override int? HueIndex => null;
-    public int R => (int) First;
-    public int G => (int) Second;
-    public int B => (int) Third;
-    public int ConstrainedR => (int) ConstrainedFirst;
-    public int ConstrainedG => (int) ConstrainedSecond;
-    public int ConstrainedB => (int) ConstrainedThird;
+    public int R => (int)First;
+    public int G => (int)Second;
+    public int B => (int)Third;
+    public int ConstrainedR => (int)ConstrainedFirst;
+    public int ConstrainedG => (int)ConstrainedSecond;
+    public int ConstrainedB => (int)ConstrainedThird;
     protected override double ConstrainedFirst => R.Clamp(0, 255);
     protected override double ConstrainedSecond => G.Clamp(0, 255);
     protected override double ConstrainedThird => B.Clamp(0, 255);

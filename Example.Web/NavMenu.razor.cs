b7ac437135208980e13cde7cf0b272b249c24d47
Wacton <waacton@gmail.com>
2025-10-27 18:32:35 +0000

    [ci skip] Experiment with nav bar

---
+++ Example.Web/NavMenu.razor.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Wacton.Unicolour.Example.Web;
+
+public partial class NavMenu : ComponentBase
+{
+}
---
+++ Example.Web/Pages/Print.razor.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Wacton.Unicolour.Example.Web.Pages;
+
+public partial class Print : ComponentBase
+{
+}
